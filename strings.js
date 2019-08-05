81a25d83ae0294f6c777da66f672a7661186c022
Stephen Senft <stephensenft21@gmail.com>
2019-08-05 13:16:09 -0500

    I have questions about the last part of this exercise

--- strings.js
+++ strings.js
@@ -8,12 +8,31 @@ return testString.split("").sort().join("")
 
 function palindrome(testString) {
     return testString == testString.split("").reverse().join("")
-
+     
     }
+    
+
+var testString = "variable,eye";
+
+let reverse = reverseString(testString);
+let alpha = alphabits(testString);
+ if (palindrome(testString)) {
+     let display = document.getElementById("display-container")
+     display.innerHTML = "Your string is a palidrome"
+ }
+
+ let displayReverse = document.getElementById("reverse-container")
+ let displayAlpha = document.getElementById("alpha-container")
+ let input = document.getElementById("input-box").addEventListener("keyup",function(event){
+     event.preventDefault()
+     if(event.keyCode === 13) {
+        document.getElementById("submit-button").click()
+     }
+ })
+  document.getElementById("submit-button")
+
+ displayReverse.innerHTML = reverse
+ displayAlpha.innerHTML = alpha
 
 
-var testString = "Hey Chris";
-reverseString(testString);
-alphabits(testString);
-palindrome(testString);
-
+ 
